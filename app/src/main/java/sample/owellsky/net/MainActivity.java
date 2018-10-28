5fb32341580733bad511aa0e678fec29e1058ef0
owellsky <33430319+owellsky@users.noreply.github.com>
2018-10-28 23:21:14 +0800

    Create MainActivity.java

---
+++ app/src/main/java/sample/owellsky/net/MainActivity.java
@@ -0,0 +1,15 @@
+package sample.owellsky.net;
+
+import android.app.*;
+import android.os.*;
+import android.support.v7.app.*;
+
+public class MainActivity extends AppCompatActivity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
